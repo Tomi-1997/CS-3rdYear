3a5bf1b772015d34e2e7b2bc5b324421b2a1e3a0
Tomi <74303735+Tomi-1997@users.noreply.github.com>
2023-03-21 14:25:20 +0200

    err redirect

--- Advanced Pr/1/shell2.c
+++ Advanced Pr/1/shell2.c
@@ -7,16 +7,18 @@
 #include "unistd.h"
 #include <string.h>
 
-int main() {
+int main() 
+{
 char command[1024];
 char *token;
 char *outfile;
-int i, fd, amper, redirect, retid, status;
+int i, fd, amper, redirect, err_redirect, retid, status;
 char *argv[10];
+char* prompt = "hello:";
 
 while (1)
 {
-    printf("hello: ");
+    printf("%s ", prompt);
     fgets(command, 1024, stdin);
     command[strlen(command) - 1] = '\0';
 
@@ -43,7 +45,8 @@ while (1)
     else 
         amper = 0; 
 
-    if (! strcmp(argv[i - 2], ">")) {
+    if (! strcmp(argv[i - 2], ">")) 
+    {
         redirect = 1;
         argv[i - 2] = NULL;
         outfile = argv[i - 1];
@@ -51,17 +54,36 @@ while (1)
     else 
         redirect = 0; 
 
+    /*Redirect error to another place*/
+    if (redirect == 0 && strcmp(argv[i - 2], "2>") == 0)
+    {
+	    err_redirect = 1;
+	    argv[i - 2] = NULL;
+	    outfile = argv[i - 1];
+    }
+    else
+    {
+	    err_redirect = 0;
+    }
+
     /* for commands not part of the shell command language */ 
 
-    if (fork() == 0) { 
+    if (fork() == 0) 
+    { 
         /* redirection of IO ? */
-        if (redirect) {
+        if (redirect) 
+	{
             fd = creat(outfile, 0660); 
             close (STDOUT_FILENO) ; 
             dup(fd); 
             close(fd); 
             /* stdout is now redirected */
         } 
+
+	/* redirection of ERR? */
+	if (err_redirect)
+	{
+	}
         execvp(argv[0], argv);
     }
     /* parent continues here */
