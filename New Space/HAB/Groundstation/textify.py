d1b56602678d3ff4c8e93c8d2c49d9f805095284
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2023-06-10 20:47:59 +0300

    prints to console aswell as writing to file

--- New Space/HAB/Groundstation/textify.py
+++ New Space/HAB/Groundstation/textify.py
@@ -1,22 +1,24 @@
-import sys, os, time
+import time
 import serial
 
 
 def monitor():
     ser = serial.Serial(COMPORT, BAUDRATE)
 
-    while (1):
+    while True:
         line = ser.readline()
         if len(line) > 0:
-            text_file = open("GS.log", "a")
             text = line.decode("utf-8")
+            text_file = open("GS.log", "a")
             text_file.write(text)
             text_file.close()
+
+            print(text, end="")
         else:
             time.sleep(1)
 
 
-COMPORT = 'COM3'
+COMPORT = 'COM5'
 BAUDRATE = 115200
 
 if __name__ == '__main__':
